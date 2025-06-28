e7eb0d9fd383ae1d10540d6f7f4a7c3ae4359fc7
oficialThur <oficialthurhenrique@gmail.com>
2025-06-28 15:10:16 -0300

    Crud: Reconfigurando o frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header.tsx';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="font-bold text-3xl text-center p-2 border-b-2 border-white text-white">
+        <h1>Crud Dev</h1>
+    </header>
+  )
+}
+
+export default Header
